61e3227effe831098b613aa6e49dd41eb438ad4e
Maxime Thizeau <maxime.thizeau@y-nov.com>
2015-02-02 14:32:02 +0100

    Get retweet status

--- script_twitter.php
+++ script_twitter.php
@@ -1,34 +1,36 @@
 <?php
 set_time_limit(0);
-include('plugins/simple_html_dom.php');
+include('simple_html_dom.php');
 
 $currentUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$url = "https://twitter.com/MaximeThizeau";
 ?>
 
 <?php
 
 
-  // S'il y a un problème avec l'URL que l'on crawl, on passe à la suivante et on met à jour en disant qu'on la faîte.
-  if (file_get_html($donnees['url']) == false OR file_get_contents($donnees['url']) == false){
-    updateUrlDoneTest($donnees['id'], $bdd);
-    continue;
-  }
+// S'il y a un problème avec l'URL que l'on crawl, on passe à la suivante et on met à jour en disant qu'on la faîte.
+if (file_get_html($url) == false OR file_get_contents($url) == false){
+  //Error
+}
 
+echo "<h2>Url crawled : ". $url.'</h2>' ;
 
+// On récupère le contenu de la page que l'on crawl.
+$html = file_get_html($url);
 
+$first_tweet = $html->find(".GridTimeline .GridTimeline-items .Grid .Grid-cell .ProfileTweet", 0);
+echo $first_tweet;
 
-  echo "<h2>Url crawled : ". $donnees['url'].'</h2>' ;
-
-  // On récupère le contenu de la page que l'on crawl.
-  $html = file_get_html($donnees['url']);
-
+$profile_context = $first_tweet->find(".ProfileTweet-header .ProfileTweet-context", 0);
+if(preg_match("#à Retweeté#", $profile_context));
+echo "OK";
 
 
 
 
 ?>
-<p>Ajout des tests fini</p>
 
 <?php
- redirectPage($currentUrl);
+// redirectPage($currentUrl);
 ?>
