4a394cbf69041ef56a72c45582c1c050324985e4
Maxime Thizeau <maxime.thizeau@y-nov.com>
2015-02-07 16:07:20 +0100

    add some css

--- test_class.php
+++ test_class.php
@@ -3,23 +3,42 @@
   include_once("autoload.inc.php");
 
 
-  $page = new Page("https://twitter.com/CosteAntonin");
+  $page = new Page("Enter Your URL Here");
   echo "<br><b>Url : </b>". $page->getUrl();
   echo "<br><b>Username : </b>". $page->getUsername();
   echo "<br><b>Login : </b>". $page->getLogin();
   echo "<br><br>";
 
-  for($i = 0; $i < 10; $i++)
+
+?>
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="Assets/css/style.css">
+</head>
+<body>
+  <?php
+  for($i = 0; $i < 4; $i++)
   {
-    echo "<br>--------------------------------";
+    echo "<div class=\"tweet\">";
     $tweet = new Tweet($page, $i);
-    echo "<br><b>Date : </b>". $tweet->getDate();
-    echo "<br><b>Author : </b>". $tweet->getAuthor();
-    echo "<br><b>Author Login: </b>". $tweet->getAuthor_login();
+    echo "<div class=\"tweet-header\">";
+    echo $tweet->getAuthor();
+    echo $tweet->getAuthor_login();
+    echo $tweet->getDate();
+    echo "</div>";
+
+
     echo "<br><b>Content : </b>". $tweet->getContent();
     foreach($tweet->getLink_array() as $link)
     {
       echo "<br><b>Link : </b>". $link;
     }
+    echo "</div>";
   }
-?>
+
+  ?>
+</body>
+</html>
